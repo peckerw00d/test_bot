af9e2115e0a0263b1e93f50b2f00a7d7a6a93ed1
inal <balkarov05@gmail.com>
2025-03-27 14:26:46 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import sys
+
+from os import getenv
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+load_dotenv()
+
+API_TOKEN = getenv("API_TOKEN")
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+async def main() -> None:
+    bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
